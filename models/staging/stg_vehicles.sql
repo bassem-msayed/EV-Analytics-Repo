e1e140dacca453963e4d77bb872f9fb7b64efe06
Bassem <Bassem.m.sayed@outlook.com>
2026-04-15 19:41:58 +0200

    feature: added a case statment to shorten the fleet_id

--- models/staging/stg_vehicles.sql
+++ models/staging/stg_vehicles.sql
@@ -9,7 +9,13 @@ with
             model,
             battery_range_km,
             age_months,
-            lower(fleet_id) as fleet_id,
+            case 
+                when trim(lower(fleet_id)) = 'fl-east' then 'east'
+                when trim(lower(fleet_id)) = 'fl-west' then 'west'
+                when trim(lower(fleet_id)) = 'fl-north' then 'north'
+                when trim(lower(fleet_id)) = 'fl-south' then 'south'
+                else trim(lower(fleet_id))
+                end as fleet_id,
             cast(procurement_date as date) as procurement_date
         from source
     )
