04895b715ad9717ae350bb4c8fa4590d7a8ce368
Bassem <Bassem.m.sayed@outlook.com>
2026-04-17 16:33:44 +0200

    feature: added de duplication logic in the intermeediate model for the subscriptions

--- models/intermediate/int_lifecycle.sql
+++ models/intermediate/int_lifecycle.sql
@@ -1,4 +1,18 @@
-with base as (
+with deduplicate_subscriptions as (
+    select 
+        *, 
+        row_number() over (
+            partition by subscription_id
+            order by start_date
+        ) as row_num
+    from {{ ref('stg_subscriptions') }}
+),
+clean_subscription as (
+    select * except row_num
+    from deduplicate_subscriptions
+    where row_num = 1
+),
+base as (
     select 
         --customer data
         c.customer_id,
@@ -22,10 +36,10 @@ with base as (
         s.monthly_fee,
         s.plan_tier,
         s.status,
-        s.cancellation_reason
+        s.cancellation_reason,
         --derived fields
         date_diff(s.end_date, s.start_date, day) as subscription_duration_days,
-        case when s.status 'cancelled' then true else false end as is_churned,
+        case when s.status = 'cancelled' then true else false end as is_churned,
         row_number() over(
             partition by s.customer_id
             order by s.start_date
